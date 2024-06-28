1efa07a3a3e81180d0884e74282ce877aad34b00
Juan Carlos <juancarloslacruzlacruz@outlook.com>
2024-06-28 13:54:37 +0200

    style postDetail is better

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,7 +8,6 @@ import { useNavigate } from 'react-router-dom';
 
 const Footer = () => {
 	const navigate = useNavigate();
-    const dispatch = useDispatch();
 	const redirect = (to) => {
 		navigate(to);
 	};
