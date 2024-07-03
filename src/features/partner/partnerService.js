a635806eec47413545648ad4eedd2574ecc7f0e2
Dani <162794640+DaniellaBarraza125@users.noreply.github.com>
2024-07-03 13:04:36 +0200

    partners

--- src/features/partner/partnerService.js
+++ src/features/partner/partnerService.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = "http://localhost:3000partners";
+const API_URL = "http://localhost:3000/partners";
 
 const getAllPartners = async () => {
     const token = localStorage.getItem("token");
