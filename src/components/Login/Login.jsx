4c5e4a524b4460ec22b94abe467f325caf9781da
Juan Carlos <juancarloslacruzlacruz@outlook.com>
2024-07-04 15:07:23 +0200

    events in login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -14,6 +14,7 @@ import {
 import Logotipo from "../../assets/Images/Logotipo.png"
 import "./Login.scss";
 import { useNavigate } from 'react-router-dom';
+import { getByDate } from '../../features/events/eventSlice';
 
 const Login = () => {
   const [formData, setFormData] = useState({
@@ -51,6 +52,7 @@ const Login = () => {
         position: "top-left"
       });
       dispatch(reset());
+      dispatch(getByDate('2025-06-25'));
     }
   }, [isSuccess, isError, msg, toast, navigate, dispatch]);
 
