924143a4bd07f9f24548c179112f26264c9b9032
Juan Carlos <juancarloslacruzlacruz@outlook.com>
2024-07-04 14:19:57 +0200

    vercel fixed 4

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default {
+export default defineConfig({
+  plugins: [react()],
   build: {
     outDir: 'build',
   }
-}
+})
+
